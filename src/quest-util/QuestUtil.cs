c8d5109a4e00010bc6575bbb09b14d0d3e13788d
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2024-04-23 15:34:41 -0300

    forgot to save

--- src/quest-util/QuestUtil.cs
+++ src/quest-util/QuestUtil.cs
@@ -1,30 +1,19 @@
-using System.Collections.Generic;
+using System;
 using MuseDotNet.Framework;
-namespace PlayerUtil
-{	
-	public interface IPlayer {
-		static public Player GetPlayerFromName(string name){
-			object namedPlayer = null;
-			Muse.ForEachPlayer((Player player) => {
-				if (player.Name == name){
-					namedPlayer = player;
+
+namespace QuestUtil
+{
+	public interface IQuest
+	{
+		public static void ForEachQuestPlayer(QuestData questData, Action<Player> callback)
+		{
+			Muse.ForEachPlayer((Player player) =>
+			{
+				if (questData.HasPlayer(player))
+				{
+					callback.Invoke(player);
 				}
 			});
-			switch(namedPlayer){
-				case Player player:
-					return player;
-				default:
-					throw new System.Exception($"no player exists of the name '{name}'");			
-			}
-		}
-		static public bool GetIfPlayerExists(string name){
-			bool playerExists = false;
-			Muse.ForEachPlayer((Player player) => {
-				if (player.Name == name){
-					playerExists = true;
-				}
-			});
-			return playerExists;
 		}
 	}
 }
---
+++ src/spawn-service/SpawnService.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using MuseDotNet.Framework;
+namespace UpgradeSystem
+{
+
+	public class SpawnService
+	{
+		private readonly Dictionary<string, Queue<Actor>> SpawnableRegistry = [];
+		private readonly List<Actor> SpawnPoints = [];
+
+		public void EnqueueSpawnable(string tag, Actor spawnable)
+		{
+			if (SpawnableRegistry.ContainsKey(tag) == false)
+			{
+				SpawnableRegistry[tag] = [];
+			}
+			spawnable.SetOnTimerCallback(() =>
+			{
+				// Debug.Log(LogLevel.Display, $"delayed deactivate = {tag}");
+				if (SpawnableRegistry.ContainsKey(tag) == true)
+				{
+					if (SpawnableRegistry[tag].Contains(spawnable))
+					{
+						spawnable.SetActive(false);
+					}
+				}
+			}, 1, 1, false);
+
+			SpawnableRegistry[tag].Enqueue(spawnable);
+		}
+
+		public bool RegisterSpawnPoint(Actor spawnPoint){
+			if (SpawnPoints.Contains(spawnPoint)){
+				SpawnPoints.Add(spawnPoint);
+				return true;
+			}
+			return false;
+		}
+
+		public Actor GetRandomSpawnPoint()
+		{
+			Actor[] spawnPoints = SpawnPoints.ToArray();
+			Random rng = new();
+
+			int index = rng.Next(0, spawnPoints.Length - 1);
+			Actor spawnPoint = spawnPoints[index];
+			return spawnPoint;
+		}
+
+		public bool Spawn(string tag, Actor spawnPoint, Action<Actor> onSpawnCallback)
+		{
+			if (SpawnableRegistry.ContainsKey(tag) == true)
+			{
+
+				Queue<Actor> spawnableQueue = SpawnableRegistry[tag];
+
+				if (spawnableQueue.Count > 0)
+				{
+					Actor spawned = spawnableQueue.Dequeue();
+					spawned.SetActive(true);
+					spawned.TeleportTo(spawned);
+					onSpawnCallback.Invoke(spawned);
+
+					return true;
+				}
+				else
+				{
+					Debug.Log(LogLevel.Warning, $"out of spawnable actors with tag {tag}");
+				}
+			}
+			return false;
+		}
+
+		// set up as singleton
+		private static readonly SpawnService instance = new();
+		public static SpawnService Instance
+		{
+			get
+			{
+				return instance;
+			}
+		}
+
+		private SpawnService() { }
+	}
+}
